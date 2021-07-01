c6b3825edbbdb8bf34f22bdbc5fa3404199290ed
Chuck Burks <r.dominica@gmail.com>
2021-07-01 09:11:27 -0700

    Deleted hold-over from previous calculations

--- Y21-06-30-plot-setup/plot_scratch1b.R
+++ Y21-06-30-plot-setup/plot_scratch1b.R
@@ -34,14 +34,14 @@ grid_ft <- grid_props*2600
 grid_ft
 # [1]  325  975 1625 2275
   # so 650 ft apart
-north_ft <- seq(from = 325, to = 3575, by = 650)
+north_ft <- seq(from = 325, to = 4700, by = 650)
 north_ft
 # [1]  325  975 1625 2275 2925 3575
 
 # Rows from northeast side
 grid_row <- (north_ft/19)%/%1
 grid_row
-# [1]  17  51  85 119  153 188
+# [1]  17  51  85 119  153 188 222
   # So 34 rows apart
 
 # Trees from north edge
@@ -53,53 +53,3 @@ grid_trees <- (east_ft/16)%/%1
 grid_trees
 # [1]  20  60 101 142 182 223 264 304
 
-rowft <- x*2817
-rowft <- cumsum(rowft)
-rowft <- rowft%/%1
-rowft
-# [1]  352 1056 1760 2464
-
-### 3 x 3 grid,  east-west (north-south rows)
-
-x <- c(0.5*1/3,1/3,1/3)
-y <- cumsum(x)
-y
-# [1] 0.1666667 0.5000000 0.8333333
-
-rows <- x*148
-rows <- rows%/%1
-# [1] 24 49 49
-rows <- cumsum(rows)
-rows
-# [1]  24  73 122
-
-trees <- x*160
-trees <- cumsum(trees)
-trees <- trees%/%1
-trees
-# [1]  26  80 133
-
-rowft <- x*2817
-rowft <- cumsum(rowft)
-rowft <- rowft%/%1
-rowft
-# [1]  469 1408 2347
-
-treeft <- x*2557
-
-### 4 x 4 grid, north-south (north-south rows)
-
-x <- c(0.125,0.25,0.25,0.25)
-y <- cumsum(x)
-y
-# [1] 0.125 0.375 0.625 0.875
-
-# Row length measured at 2557. Tree space estimated ast 16. Tree per row:
-trees_pr_row <- (2557/16)%/%1
-trees_pr_row
-# [1] 159
-trees_pr_row <- 160 # round estimate
-
-trees <- x*160
-trees <- cumsum(trees)
-trees
